99be0d6f1b2f41408c3e38648966ee12d4ccc98d
james <github email address>
2017-07-21 17:26:18 -0500

    Adds update action the users controller

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -52,4 +52,36 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+    context "when is succefully updated" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                                 user: { email: "newmail@example.com" } }, format: :json
+      end
+
+      it "renders the json representation for the updated user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql("newmail@example.com")
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: { id: @user.id,
+                                 user: { email: "bademail.com" } }, format: :json
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include("is invalid")
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
