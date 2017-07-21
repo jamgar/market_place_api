d971f6749374dd130387c2e644525d6cee4ae4e2
james <github email address>
2017-07-21 17:09:03 -0500

    Adds the user create endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::API
   # Prevents CSRF attacks by raising an exception.
-  protect_from_forgery with: :null_session
+#  protect_from_forgery with: :null_session
 end
