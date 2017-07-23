2979e5734dabd8f1bb077e44de3012008d9c38ca
james <github email address>
2017-07-23 16:57:16 -0500

    Adds user_signed_in? method to know whether the user is logged in or not

--- spec/controllers/concerns/authenticable_spec.rb
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -31,9 +31,29 @@ describe Authenticable do
     end
 
     it "render a json error message" do
-      expect(json_response[:erros]).to eql("Not authenticated")
+      expect(json_response[:errors]).to eql("Not authenticated")
     end
 
     it { should respond_with 401 }
+  end
+
+  describe "#user_signed_in?" do
+    context "when there is a user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        allow(authentication).to receive(:current_user).and_return(@user)
+      end
+
+      it { should be_user_signed_in }
+    end
+
+    context "when there is no user on 'session'" do
+      before do
+        @user = FactoryGirl.create :user
+        allow(authentication).to receive(:current_user).and_return(nil)
+      end
+
+      it { should_not be_user_signed_in }
+    end
   end
 end
